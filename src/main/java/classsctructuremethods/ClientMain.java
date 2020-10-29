da3f2929919ce3f4b1e6d980468f8ac44b2a9b39
jfulop99 <janos.fulop.mail@gmail.com>
2020-10-29 20:49:34 +0100

    Method1

---
+++ src/main/java/classsctructuremethods/ClientMain.java
@@ -0,0 +1,20 @@
+package classsctructuremethods;
+
+public class ClientMain {
+    public static void main(String[] args) {
+        Client client = new Client();
+        // Set parameters
+        client.setName("Maci Laci");
+        client.setYear(1967);
+        client.setAddress("Kerekerdő");
+        // Print parameters
+        System.out.println("Name: " + client.getName());
+        System.out.println("DoB : " + client.getYear());
+        System.out.println("Addr: " + client.getAddress());
+        // Change address
+        client.migrate("Négyszögletes kerek erdő");
+        // Print new address
+        System.out.println("New address: " + client.getAddress());
+
+    }
+}
