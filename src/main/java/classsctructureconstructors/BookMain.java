b2bd2def2c137579f2afdb4e810e83e3a0aad37a
jfulop99 <janos.fulop.mail@gmail.com>
2020-10-29 21:40:28 +0100

    Constructor1

---
+++ src/main/java/classsctructureconstructors/BookMain.java
@@ -0,0 +1,10 @@
+package classsctructureconstructors;
+
+public class BookMain {
+    public static void main(String[] args) {
+        Book book = new Book("Rejtő Jenő", "14 karátos autó");
+        book.regNumber("112-234-5678");
+
+        System.out.println("Author: " + book.getAuthor() + " Title: " + book.getTitle() + " RegNumber: " + book.getRegNumber());
+    }
+}
