9bfab56b547e3eaf73a3ddb3333ffa4c5e3ed4f1
jfulop99 <janos.fulop.mail@gmail.com>
2020-10-29 17:45:21 +0100

    Calculator

--- src/main/java/classstructureio/Calculator.java
+++ src/main/java/classstructureio/Calculator.java
@@ -6,9 +6,9 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Adj meg két számot !");
-        int operand1 = scanner.nextInt();
-        int operand2 = scanner.nextInt();
-        System.out.println(operand1 + " + " + operand2);
-        System.out.println(operand1 + operand2);
+        int firstOperand = scanner.nextInt();
+        int secondOperand = scanner.nextInt();
+        System.out.println(firstOperand + " + " + secondOperand);
+        System.out.println(firstOperand + secondOperand);
     }
 }
