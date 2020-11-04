a79b90952b9b18d9a11a02c43e79eebd79050947
jfulop99 <janos.fulop.mail@gmail.com>
2020-11-04 13:14:58 +0100

    week2

---
+++ src/main/java/week02/Product.java
@@ -0,0 +1,41 @@
+package week02;
+
+public class Product {
+    private String name;
+    private String code;
+
+    public Product(String name, String code) {
+        this.name = name;
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public boolean areTheyEqual(Product product){
+        return name.equals(product.getName());
+    }
+
+    public static void main(String[] args) {
+        Product prduct1 = new Product("Bicikli", "12345678");
+        Product prduct2 = new Product("Kerékpár", "87654321");
+
+
+        System.out.println(prduct1.areTheyEqual(prduct2));
+        prduct2.setName("Bicikli");
+        System.out.println(prduct1.areTheyEqual(prduct2));
+    }
+}
