719c6fde033652faf9abba55eba942fb8069bc86
jfulop99 <janos.fulop.mail@gmail.com>
2020-11-06 22:14:12 +0100

    RoundingAnomaly

--- src/main/java/math/RoundingAnomaly.java
+++ src/main/java/math/RoundingAnomaly.java
@@ -1,30 +1,53 @@
 package math;
 
-import java.lang.reflect.Array;
-import java.math.RoundingMode;
-import java.text.DecimalFormat;
 import java.util.Random;
 
 public class RoundingAnomaly {
-    private double randomNumbers [];
-    private final Random random = new Random();
+    private double[] randomNumbers;
+    private Random random = new Random();
 
 
     public double[] randomNumbers(int size, double max, int scale){
         randomNumbers = new double[size];
         for (int i = 0; i < size; i++){
-            DecimalFormat decimalFormat = new DecimalFormat();
-            decimalFormat.setRoundingMode(RoundingMode.CEILING);
-            randomNumbers [i] = random.nextDouble() * 1000000.0;
-
-
+            randomNumbers [i] = Math.round(random.nextDouble() * max * Math.pow(10, scale)) / Math.pow(10, scale);
         }
         return randomNumbers;
     }
 
+    public double roundAfterSum(double[] randomNumbers){
+        double sum = 0.0;
+        for (double number:randomNumbers) {
+            sum += number;
+        }
+        return Math.round(sum);
+    }
+
+    public double sumAfterRound(double[] randomNumbers){
+        double sum = 0.0;
+        for (double number:randomNumbers) {
+            sum += Math.round(number);
+        }
+        return sum;
+    }
+
+    public double difference(int size, double max, int scale){
+        double[] randomArray;
+        randomArray = randomNumbers(size, max, scale);
+        return Math.abs(roundAfterSum(randomNumbers) - sumAfterRound(randomNumbers));
+    }
+
+
     public static void main(String[] args) {
+
         RoundingAnomaly roundingAnomaly = new RoundingAnomaly();
-        roundingAnomaly.randomNumbers(100, 1000000.0, 5);
+        double sum = 0.0;
+        for (int i = 0; i < 100; i++) {
+            double difference = roundingAnomaly.difference(1000, 1000000, 5);
+            sum += difference;
+            System.out.println("Difference: " + difference);
+        }
+        System.out.println("Mean difference: " + sum / 100);
     }
 
 }
