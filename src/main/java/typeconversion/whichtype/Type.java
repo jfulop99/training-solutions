0cfdacbea0f1a28ac0900a69380c9bb8eca1e018
jfulop99 <janos.fulop.mail@gmail.com>
2020-11-09 21:07:09 +0100

    typeconversion

--- src/main/java/typeconversion/whichtype/Type.java
+++ src/main/java/typeconversion/whichtype/Type.java
@@ -2,7 +2,7 @@ package typeconversion.whichtype;
 
 public enum Type {
 
-    BYTE(Byte.MIN_VALUE, Byte.MAX_VALUE), SHORT(Short.MIN_VALUE, Short.MAX_VALUE), INT(Long.MIN_VALUE, Long.MAX_VALUE);
+    BYTE(Byte.MIN_VALUE, Byte.MAX_VALUE), SHORT(Short.MIN_VALUE, Short.MAX_VALUE), INT(Integer.MIN_VALUE, Integer.MAX_VALUE);
 
     private final long minValue;
     private final long maxValue;
--- src/main/java/typeconversion/whichtype/WhichTypeMain.java
+++ src/main/java/typeconversion/whichtype/WhichTypeMain.java
@@ -2,6 +2,11 @@ package typeconversion.whichtype;
 
 public class WhichTypeMain {
     public static void main(String[] args) {
+        System.out.println(WhichType.whichType(Long.toString(Long.MAX_VALUE - 2)));
+        System.out.println(WhichType.whichType(Long.toString((long)Integer.MAX_VALUE - 2)));
+        System.out.println(WhichType.whichType(Long.toString((long)Short.MAX_VALUE - 2)));
+        System.out.println(WhichType.whichType(Long.toString((long)Byte.MAX_VALUE - 2)));
+        System.out.println(WhichType.whichType(Long.toString(0)));
         System.out.println(WhichType.whichType("123"));
     }
 }
