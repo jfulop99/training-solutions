f5868a7acd80c7e9da080022dee4a35f823801aa
jfulop99 <janos.fulop.mail@gmail.com>
2020-11-12 17:56:45 +0100

    introexception

---
+++ src/main/java/introexception/patient/Patient.java
@@ -0,0 +1,42 @@
+package introexception.patient;
+
+public class Patient {
+    String name;
+    String socialSecurityNumber;
+    int yearOfBirth;
+
+    public Patient(String name, String socialSecurityNumber, int yearOfBirth) {
+        SsnValidator ssnValidator = new SsnValidator();
+        if (name.length() > 0){
+            this.name = name;
+        }
+        else {
+            throw new IllegalArgumentException();
+        }
+        if (ssnValidator.isValidSsn(socialSecurityNumber)){
+            this.socialSecurityNumber = socialSecurityNumber;
+        }
+        else {
+            throw new IllegalArgumentException();
+        }
+
+        if (yearOfBirth >= 1900){
+            this.yearOfBirth = yearOfBirth;
+        }
+        else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+}
---
+++ src/main/java/introexception/patient/SsnValidator.java
@@ -0,0 +1,31 @@
+package introexception.patient;
+
+public class SsnValidator {
+    public boolean isValidSsn(String ssn){
+        boolean valid = true;
+        try {
+            Integer.parseInt(ssn);
+        } catch (IllegalArgumentException e) {
+            valid = false;
+            //throw new IllegalArgumentException();
+        }
+        if (valid){
+            valid = (ssn.length() != 9) ? false : true;
+        }
+        if (valid) {
+            int checkSum = 0;
+            for (int i = 0; i < ssn.length()-1; i++){
+                if(i % 2 == 0) {
+                    checkSum += (Integer.parseInt(ssn.substring(i, i+1)) * 3);
+                }
+                else {
+                    checkSum += (Integer.parseInt(ssn.substring(i, i+1)) * 10);
+                }
+            }
+            if ( checkSum % 10 != (Integer.parseInt(ssn.substring(8)))) {
+                valid = false;
+            }
+        }
+        return valid;
+    }
+}
