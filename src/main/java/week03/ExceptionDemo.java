a281740fb7ebbe10656818eef0b5049f3ee81265
jfulop99 <janos.fulop.mail@gmail.com>
2020-11-13 22:08:20 +0100

    week03

---
+++ src/main/java/week03/ExceptionDemo.java
@@ -0,0 +1,15 @@
+package week03;
+
+public class ExceptionDemo {
+
+    public static void main(String[] args) {
+
+//        System.out.println(1 / 0);
+
+//        String s = null;
+//        System.out.println(s.toLowerCase());
+
+
+
+    }
+}
