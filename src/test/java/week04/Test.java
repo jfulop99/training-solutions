7557ac30467763977f3f049385d416e24133c25d
jfulop99 <janos.fulop.mail@gmail.com>
2020-11-16 19:09:00 +0100

    teszt

---
+++ src/test/java/week04/Test.java
@@ -0,0 +1,27 @@
+package week04;
+
+import java.util.Scanner;
+
+public class Test {
+
+    public String filterLinesWithWordOccurrences(String text, String word) {
+        String line = "";
+        Scanner scanner = new Scanner(text);
+        while (scanner.hasNextLine()) {
+            String loopLine = scanner.nextLine();
+//            while (scanner.hasNext()) {
+//                String s = scanner.next();
+                if (loopLine.contains(word)) {
+                    line += loopLine + "\n";
+                }
+            }
+//        }
+        return line;
+    }
+
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        System.out.println( test.filterLinesWithWordOccurrences("alma word\nldlsdnlds\nkorte word\n", "word"));
+    }
+}
