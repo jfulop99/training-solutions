d41e1016e04cc6339ec743ebad94872f10fadc17
jfulop99 <janos.fulop.mail@gmail.com>
2020-11-28 15:33:59 +0100

    Hackerrank static initializer

---
+++ src/test/java/week05d05/SolutionTest.java
@@ -0,0 +1,41 @@
+package week05d05;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import week04.Main;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    @DisplayName("Test the main method")
+    void mainTest() {
+
+
+//        String input = "3\n4\n";
+//        String expected = "12";
+
+        String input = "0\n4\n";
+        String expected = "java.lang.Exception: Breadth and height must be positive\r\n";
+
+
+
+        // set stdin
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        // set stdout
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(baos);
+        System.setOut(ps);
+
+
+        week05d05.Solution.main(null);
+        assertEquals(expected, baos.toString());
+
+    }
+}
