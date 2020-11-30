b997553b90b6ce250bcfc20436cc207bd52ad493
jfulop99 <janos.fulop.mail@gmail.com>
2020-11-30 12:41:32 +0100

    week06d01_Junior/Mid

---
+++ src/main/java/week06d01/ListSelector.java
@@ -0,0 +1,22 @@
+package week06d01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListSelector {
+
+    public String evenSelector(List<String> inputList) {
+        if (inputList == null) {
+            throw new IllegalArgumentException("Input list is null");
+        }
+        int i = 0;
+        List<String> outputList = new ArrayList<>();
+        for (String item: inputList) {
+            if (i % 2 == 0) {
+                outputList.add(item);
+            }
+            i++;
+        }
+        return outputList.toString();
+    }
+}
