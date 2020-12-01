6556292ae9fb4f73b116f07771935ae9724960d0
jfulop99 <janos.fulop.mail@gmail.com>
2020-12-01 10:20:25 +0100

    week06d02_Senior

---
+++ src/main/java/week06d02/Product.java
@@ -0,0 +1,26 @@
+package week06d02;
+
+public class Product {
+
+    private String name;
+    private CategoryOfProduct categoryOfProduct;
+    private double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public CategoryOfProduct getCategoryOfProduct() {
+        return categoryOfProduct;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public Product(String name, CategoryOfProduct categoryOfProduct, double price) {
+        this.name = name;
+        this.categoryOfProduct = categoryOfProduct;
+        this.price = price;
+    }
+}
---
+++ src/test/java/week06d02/StoreTest.java
@@ -0,0 +1,32 @@
+package week06d02;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StoreTest {
+
+    @Test
+    void getProductsByCategory() {
+
+        Store store = null;
+
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("Krumpli", CategoryOfProduct.FROZEN, 25.65));
+        products.add(new Product("Krumpli", CategoryOfProduct.DAIRY, 25.65));
+        products.add(new Product("Krumpli", CategoryOfProduct.DAIRY, 25.65));
+        products.add(new Product("Krumpli", CategoryOfProduct.FROZEN, 25.65));
+        products.add(new Product("Krumpli", CategoryOfProduct.FROZEN, 25.65));
+        products.add(new Product("Krumpli", CategoryOfProduct.BAKEDGOODS, 25.65));
+
+        store = new Store(products);
+
+        assertEquals(3, store.getProductsByCategory().get(0).getNumber());
+        assertEquals(2, store.getProductsByCategory().get(1).getNumber());
+        assertEquals(1, store.getProductsByCategory().get(2).getNumber());
+    }
+}
