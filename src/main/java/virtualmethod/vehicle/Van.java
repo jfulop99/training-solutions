8cce07b00627dcc7042102f94e3a66ec5c022896
jfulop99 <janos.fulop.mail@gmail.com>
2020-12-06 20:48:58 +0100

    virtualmethod

---
+++ src/main/java/virtualmethod/vehicle/Van.java
@@ -0,0 +1,23 @@
+package virtualmethod.vehicle;
+
+public class Van extends Car{
+
+    int cargoWeight;
+
+    public Van(int numberOfPassenger, int vehicleWeight, int cargoWeight) {
+        super(vehicleWeight, numberOfPassenger);
+        this.cargoWeight = cargoWeight;
+    }
+
+    @Override
+    public int getGrossLoad() {
+        return super.getGrossLoad() + cargoWeight;
+    }
+
+    @Override
+    public String toString() {
+        return "Van{" +
+                "cargoWeight=" + cargoWeight + ", numberOfPassenger=" + getNumberOfPassenger() + ", vehicleWeight=" + getVehicleWeight() +
+                '}';
+    }
+}
