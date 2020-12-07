226e08279b2f827958e9c111f2a9d3378dbd98dd
jfulop99 <janos.fulop.mail@gmail.com>
2020-12-07 12:10:49 +0100

    week07d01

---
+++ src/test/java/week07d01/FibonacciTest.java
@@ -0,0 +1,16 @@
+package week07d01;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FibonacciTest {
+
+    @Test
+    void fibonacciTest() {
+        assertEquals(13, Fibonacci.fib(7));
+        assertEquals(13, Fibonacci.fib2(7));
+
+    }
+
+}
