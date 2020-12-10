03c9247798006b8e677bffbd0c573b8a75e5df3b
jfulop99 <janos.fulop.mail@gmail.com>
2020-12-10 16:03:29 +0100

    week07d04

---
+++ src/main/java/week07d04/Lab.java
@@ -0,0 +1,81 @@
+package week07d04;
+
+import java.time.LocalDate;
+
+public class Lab {
+
+    String title;
+    boolean completed = false;
+    LocalDate completedAt;
+
+    public Lab(String title) {
+        if (isNullOrEmpty(title)) {
+            throw new IllegalArgumentException("Title is not valid");
+        }
+        this.title = title;
+    }
+
+    public Lab(String title, LocalDate completedAt) {
+        if (isNullOrEmpty(title)) {
+            throw new IllegalArgumentException("Title is not valid");
+        }
+        this.title = title;
+        this.completedAt = completedAt;
+        completed = true;
+    }
+
+    public void complete(){
+        completed = true;
+        completedAt = LocalDate.now();
+    }
+
+    public void complete(LocalDate date){
+        completed = true;
+        completedAt = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return "Lab{" +
+                "title='" + title + '\'' +
+                ", completed=" + completed +
+                ", completedAt=" + completedAt +
+                '}';
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public LocalDate getCompletedAt() {
+        return completedAt;
+    }
+
+    private boolean isNullOrEmpty(String title) {
+        return (title == null || title.isBlank());
+    }
+
+    public static void main(String[] args) {
+        Lab lab1 = new Lab("Exception");
+        System.out.println(lab1);
+        Lab lab2 = new Lab("Inheritance", LocalDate.of(2020, 12, 06));
+        System.out.println(lab2);
+        Lab lab3 = new Lab("Exception");
+        System.out.println(lab3);
+
+        lab1.complete();
+        lab3.complete(LocalDate.of(2020,12,9));
+
+        System.out.println(lab1);
+
+        System.out.println(lab2);
+
+        System.out.println(lab3);
+
+    }
+
+}
---
+++ src/test/java/week07d04/LabTest.java
@@ -0,0 +1,43 @@
+package week07d04;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LabTest {
+
+
+    @Test
+    void constructorTest() {
+        Lab lab1 = new Lab("Exception");
+        Lab lab2 = new Lab("Inheritance", LocalDate.of(2020, 12, 06));
+        Lab lab3 = new Lab("Exception");
+        assertFalse(lab1.isCompleted());
+        assertTrue(lab2.isCompleted());
+        assertFalse(lab3.isCompleted());
+        assertEquals(lab1.getTitle(), "Exception");
+        assertEquals(lab2.getTitle(), "Inheritance");
+        assertEquals(lab3.getTitle(), "Exception");
+        assertEquals(null, lab1.getCompletedAt());
+        assertEquals(LocalDate.of(2020,12,06), lab2.getCompletedAt());
+        assertEquals(null, lab3.getCompletedAt());
+
+    }
+
+    @Test
+    void completedTest() {
+        Lab lab1 = new Lab("Exception");
+        Lab lab2 = new Lab("Inheritance", LocalDate.of(2020, 12, 06));
+        Lab lab3 = new Lab("Exception");
+        lab1.complete();
+        lab3.complete(LocalDate.of(2020,12,9));
+        assertTrue(lab1.isCompleted());
+        assertTrue(lab2.isCompleted());
+        assertTrue(lab3.isCompleted());
+        assertEquals(LocalDate.now(), lab1.getCompletedAt());
+        assertEquals(LocalDate.of(2020,12,06), lab2.getCompletedAt());
+        assertEquals(LocalDate.of(2020,12,9), lab3.getCompletedAt());
+    }
+}
