ee7370d7c08f6823339940d5a69c849d5a73f4dc
jfulop99 <janos.fulop.mail@gmail.com>
2020-12-17 11:29:09 +0100

    week08d04 senior V2

--- src/main/java/week08/d04/ExamplesStore.java
+++ src/main/java/week08/d04/ExamplesStore.java
@@ -8,22 +8,34 @@ import java.util.List;
 
 public class ExamplesStore {
 
-    private List<String> titlesOfExamples = new ArrayList<>();
+    private List<String> titlesOfExamples;
+    private static final String MARK_OF_TITLE= "#";
+
+    public ExamplesStore() {
+        titlesOfExamples = new ArrayList<>();
+    }
 
     public List<String> getTitlesOfExamples() {
 
         String line;
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(ExamplesStore.class.getResourceAsStream("examples.md")))){
             while ((line = reader.readLine()) != null){
-                if (line.startsWith("#")) {
-                    titlesOfExamples.add(line.substring(1));
-                }
+                addTitles(line);
             }
 
         } catch (IOException e) {
             throw new IllegalStateException("Cannot read file", e);
+        } catch (NullPointerException e) {
+            throw new IllegalStateException("File not found", e);
         }
         return titlesOfExamples;
+    }
+
+    private void addTitles(String line) {
+        if (line.startsWith(MARK_OF_TITLE)) {
+            String lineWithoutMark = line.substring(2);
+            titlesOfExamples.add(lineWithoutMark);
+        }
     }
 
     public static void main(String[] args) {
