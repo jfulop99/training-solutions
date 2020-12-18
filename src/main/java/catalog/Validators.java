f22c2026b4af377ec66819a06e742b53d0c9f6c6
jfulop99 <janos.fulop.mail@gmail.com>
2020-12-18 08:58:58 +0100

    3. Projektmunka

--- src/main/java/catalog/Validators.java
+++ src/main/java/catalog/Validators.java
@@ -8,7 +8,7 @@ public class Validators {
         return str == null || str.isBlank();
     }
 
-    public static boolean isEmpty(List<String> list) {
+    public static boolean isEmpty(List list) {
         return list == null || list.isEmpty();
     }
 
