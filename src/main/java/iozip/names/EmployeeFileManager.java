6ad50fec9e06117394be0c73023788b6d75a44fc
jfulop99 <janos.fulop.mail@gmail.com>
2020-12-18 09:08:37 +0100

    iozip

---
+++ src/main/java/iozip/names/EmployeeFileManager.java
@@ -0,0 +1,12 @@
+package iozip.names;
+
+import java.nio.file.Path;
+import java.util.List;
+
+public class EmployeeFileManager {
+
+    public void saveEmployees(Path path, List<String> names) {
+
+    }
+
+}
