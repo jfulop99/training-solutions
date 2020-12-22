11a3f50214b1f1010cbe69833836ebc5a68bfc70
jfulop99 <janos.fulop.mail@gmail.com>
2020-12-22 11:29:55 +0100

    week09d02

---
+++ src/main/java/week09/d02/FibCalculator.java
@@ -0,0 +1,56 @@
+package week09.d02;
+
+public class FibCalculator {
+
+
+    public long sumEvens(int bound) {
+        long result = 0;
+        long fibonacci = 0;
+        int count = 1;
+
+        while ((fibonacci = fib(count)) < bound) {
+            if (fibonacci % 2 == 0) {
+                result += fibonacci;
+            }
+            count++;
+        }
+        return result;
+    }
+
+    private long fib(int n) {
+        long result = 0;
+        long x = 1;
+        long y = 1;
+        for (int i = 0; i < n; i++) {
+            result = y;
+            long temp =  x + y;
+            y = x;
+            x = temp;
+        }
+        return result;
+    }
+
+    public long sumEvens2(int bound) {
+            long result = 0;
+            long x = 1;
+            long y = 1;
+            long sum = 0;
+            do {
+                result = y;
+                long temp =  x + y;
+                y = x;
+                x = temp;
+                if (result % 2 == 0 && result < bound) {
+                    sum += result;
+                }
+            }while (result < bound);
+            return sum;
+    }
+
+
+    public static void main(String[] args) {
+        FibCalculator fibonacci = new FibCalculator();
+        System.out.println(fibonacci.sumEvens(100));
+        System.out.println(fibonacci.sumEvens2(100));
+    }
+}
