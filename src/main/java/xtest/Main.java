a8182cafb3d71a3e3e4980e924bd76f33eff5188
jfulop99 <janos.fulop.mail@gmail.com>
2021-01-07 20:42:02 +0100

    test

--- src/main/java/xtest/Main.java
+++ src/main/java/xtest/Main.java
@@ -3,12 +3,16 @@ package xtest;
 import week08.d05.MathAlgorithms;
 
 public class Main {
-
+    public static String convert(int a, int b) {
+        return Integer.toString(a) + Integer.toString(b);
+    }
 
     public static void main(String[] args) {
         B c = new C();
 
         System.out.println(c instanceof A);
+
+        System.out.println(new Main().convert(5,6));
     }
 
 }
