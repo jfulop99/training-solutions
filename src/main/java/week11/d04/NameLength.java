25b97bab8adf6efe4387f4cf97cab23e35298da5
jfulop99 <janos.fulop.mail@gmail.com>
2021-01-14 12:45:29 +0100

    week11 d04 NameLength végleges null ellenőrzés és tesztek

--- src/main/java/week11/d04/NameLength.java
+++ src/main/java/week11/d04/NameLength.java
@@ -7,19 +7,22 @@ import java.util.Set;
 
 public class NameLength {
 
-    public static final String FIRSTLETTER = "J";
+    public static final String FIRST_LETTER = "J";
 
-    public static List<Integer> getLengths(List<String> names) {
+    public List<Integer> getLengths(List<String> names) {
         Set<Integer> lengthsOfNames = new HashSet<>();
-        for (String item: names) {
-            if (item.substring(0, 1).equals(FIRSTLETTER)) {
-                lengthsOfNames.add(item.length());
+        if (names != null) {
+            for (String item : names) {
+                if (item != null && item.toUpperCase().startsWith(FIRST_LETTER)) {
+                    lengthsOfNames.add(item.length());
+                }
             }
         }
     return new ArrayList<>(lengthsOfNames);
     }
 
     public static void main(String[] args) {
-        System.out.println(NameLength.getLengths(List.of("Joe", "Jack", "Jane", "Jake", "George", "William")));
+
+        System.out.println(new NameLength().getLengths(List.of("Joe", "Jack", "Jane", "Jake", "George", "William")));
     }
 }
--- src/test/java/week11/d04/NameLengthTest.java
+++ src/test/java/week11/d04/NameLengthTest.java
@@ -3,6 +3,7 @@ package week11.d04;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,6 +12,22 @@ class NameLengthTest {
 
     @Test
     void getLengths() {
-        assertEquals(Arrays.asList(3, 4), NameLength.getLengths(List.of("Joe", "Jack", "Jane", "Jake", "George", "William")));
+        List<Integer> length = new NameLength().getLengths(List.of("Joe", "Jack", "Jane", "Jake", "George", "William"));
+        Collections.sort(length);
+        assertEquals(Arrays.asList(3, 4),length );
     }
+
+    @Test
+    void getLengthsWithNullList() {
+        List<Integer> length = new NameLength().getLengths(null);
+        assertEquals(0, length.size());
+    }
+
+    @Test
+    void getLengthsWithNullItem() {
+        List<Integer> length = new NameLength().getLengths(Arrays.asList("Joe", "Jack", "Jane", "Jake", null, "William"));
+        Collections.sort(length);
+        assertEquals(Arrays.asList(3, 4),length);
+    }
+
 }
