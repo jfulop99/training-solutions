f6e638870a96cf75fd277c2c259ab9ce698d821c
jfulop99 <janos.fulop.mail@gmail.com>
2021-02-02 20:26:38 +0100

    datenewtypes Rendezvous

--- src/main/java/datenewtypes/Rendezvous.java
+++ src/main/java/datenewtypes/Rendezvous.java
@@ -1,30 +1,68 @@
 package datenewtypes;
 
-import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Locale;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoUnit;
 
 public class Rendezvous {
 
-    private LocalDateTime rendezvousDate;
+    private LocalTime dateTime;
 
 
-    public Rendezvous(String dateString, String format, Locale locale) {
+    public Rendezvous(int hours, int minutes) {
+
+        dateTime = LocalTime.of(hours, minutes);
+
+    }
+
+    public Rendezvous(String dateTimeString, String format) {
         if (format == null || format.isBlank()) {
-            throw new IllegalArgumentException("Empty pattern string, cannot use: ");
+            throw new IllegalArgumentException("Empty pattern string!");
         }
-        if (locale == null) {
-            throw new NullPointerException("Locale must not be null!");
+        if (dateTimeString == null || dateTimeString.isBlank()) {
+            throw new IllegalArgumentException("Illegal time string: ");
         }
 
-        rendezvousDate = LocalDateTime.parse(dateString, DateTimeFormatter.ofPattern(format, locale));
+        try {
+            dateTime = LocalTime.parse(dateTimeString, DateTimeFormatter.ofPattern(format));
+        }catch (DateTimeParseException e) {
+            throw new IllegalArgumentException("Illegal time string: " + dateTimeString, e);
+        }
     }
 
-    public void setRendezvousDate(LocalDateTime rendezvousDate) {
-        this.rendezvousDate = rendezvousDate;
+    public void setDateTime(LocalTime dateTime) {
+        this.dateTime = dateTime;
     }
 
-    public LocalDateTime getRendezvousDate() {
-        return rendezvousDate;
+    public LocalTime getDateTime() {
+        return dateTime;
+    }
+
+    public String toString(String format) {
+        if (format == null || format.isBlank()) {
+            throw new IllegalArgumentException("Empty pattern string!");
+        }
+        return dateTime.format(DateTimeFormatter.ofPattern(format));
+    }
+
+    public void pushLater(int hours, int minutes) {
+
+        dateTime = dateTime.plusHours(hours).plusMinutes(minutes);
+
+    }
+
+    public void pullEarlier(int hours, int minutes) {
+
+        dateTime = dateTime.minusHours(hours).minusMinutes(minutes);
+
+    }
+
+    public long countMinutesLeft(LocalTime base) {
+        long result = ChronoUnit.MINUTES.between(base, dateTime);
+        if (result < 0) {
+            throw new MissedOpportunityException("Too late!");
+        }
+        return result;
     }
 }
