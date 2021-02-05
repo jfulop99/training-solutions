32991ecd1a5dde7c3206f87579ccd5f422f58c39
jfulop99 <janos.fulop.mail@gmail.com>
2021-02-05 12:28:13 +0100

    week14d05 Junior/Mid

--- src/main/java/week14/d05/Junior.java
+++ src/main/java/week14/d05/Junior.java
@@ -2,6 +2,7 @@ package week14.d05;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
@@ -11,26 +12,47 @@ public class Junior {
 
     private Map<String, Integer> result = new HashMap<>();
 
-    public Map<String, Integer> countWords(BufferedReader reader, String... words) throws IOException {
+    public Map<String, Integer> countWords(Path path, String... words) {
 
-        String line = "";
-        while ((line = reader.readLine()) != null) {
-
-            for (String word : words) {
-                if (line.contains(word)) {
-                    result.merge(word, 1, Integer::sum);
-                }
-            }
+        try (BufferedReader reader = Files.newBufferedReader(path, Charset.forName("windows-1250"))) {
+            lineProcessor(reader, words);
+        } catch (IOException e) {
+            throw new IllegalStateException("Cannot read file", e);
         }
-
         return result;
     }
 
+    public void lineProcessor(BufferedReader reader, String[] words) throws IOException {
+        String line = "";
+        while ((line = reader.readLine()) != null) {
+            for (String word : words) {
+                if (line.contains(word)) {
+                    int count = 1;
+                    count = inLineWordCounter(line, word); // Ha kell soron belül számolni
+                    result.merge(word, count, Integer::sum);
+                }
+            }
+        }
+    }
+
+    private int inLineWordCounter(String line, String find) {
+
+        String[] parts = line.split("\\s");
+        int counter = 0;
+        for (String word : parts) {
+            if (word.contains(find)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+
     public static void main(String[] args) {
         Junior junior = new Junior();
-        Path path = Path.of("c:\\Training\\training-solutions\\hachiko1.srt");
-        try (BufferedReader reader = Files.newBufferedReader(path)) {
-            System.out.println(junior.countWords(reader, "Hachi", "haza", "pályaudvar", "jó", "Hachiko"));
+        Path path = Path.of("hachiko.srt");
+        try (BufferedReader reader = Files.newBufferedReader(path, Charset.forName("windows-1250"))) {
+            System.out.println(junior.countWords(path, "Hachiko", "haza", "pályaudvar", "jó", "Hach"));
         } catch (IOException e) {
             throw new IllegalStateException("Cannot read file", e);
         }
