ed1d2ce8a0088747abf9ef0af7817975e11c8883
jfulop99 <janos.fulop.mail@gmail.com>
2021-02-12 22:14:36 +0100

    week15d05 Junior

--- src/main/java/week15/d05/GameOfThrones.java
+++ src/main/java/week15/d05/GameOfThrones.java
@@ -1,6 +1,7 @@
 package week15.d05;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
@@ -12,16 +13,18 @@ import java.util.stream.Stream;
 
 public class GameOfThrones {
 
+    private static Stream<String> getHousesFromLine(String s) {
+        String[] parts = s.split(",");
+        return Stream.of(parts).skip(5).limit(8);
+    }
+
     public Optional<Map.Entry<String, Long>> maxBattlePerHouse(Path path) {
         Map<String, Long> houses = new HashMap<>();
 
-        try (Stream<String> lines = Files.lines(path)) {
+        try (Stream<String> lines = Files.lines(path, StandardCharsets.UTF_8)) {
             houses = lines
                     .skip(1)
-                    .flatMap(s -> {
-                        String[] parts = s.split(",");
-                        return Stream.of(parts).skip(5).limit(8);
-                    })
+                    .flatMap(GameOfThrones::getHousesFromLine)
                     .filter(a -> !a.isBlank())
                     .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
