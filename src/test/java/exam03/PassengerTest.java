46b762a9e7154ce51382e9aab32e1034fff430d0
jfulop99 <janos.fulop.mail@gmail.com>
2021-02-15 16:53:39 +0100

    exam03 Start commit

---
+++ src/test/java/exam03/PassengerTest.java
@@ -0,0 +1,11 @@
+package exam03;
+
+public class PassengerTest {
+
+//    @Test
+//    void create() {
+//        Passenger passenger = new Passenger("John Doe", CruiseClass.LUXURY);
+//        assertEquals("John Doe", passenger.getName());
+//        assertEquals(CruiseClass.LUXURY, passenger.getCruiseClass());
+//    }
+}
