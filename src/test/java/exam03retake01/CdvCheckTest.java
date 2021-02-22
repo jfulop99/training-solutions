15582ba74b2c6ed9f927b4d7ff202d9e83032168
jfulop99 <janos.fulop.mail@gmail.com>
2021-02-22 16:56:05 +0100

    Exam03retake01

---
+++ src/test/java/exam03retake01/CdvCheckTest.java
@@ -0,0 +1,19 @@
+package exam03retake01;
+
+class CdvCheckTest {
+
+//    @Test
+//    void check() {
+//        assertTrue(new CdvCheck().check("8365670003"));
+//        assertFalse(new CdvCheck().check("8365670004"));
+//    }
+//
+//    @Test
+//    void notNumber() {
+//        assertThrows(IllegalArgumentException.class,
+//                () -> new CdvCheck().check("123456789"));
+//
+//        assertThrows(IllegalArgumentException.class,
+//                () -> new CdvCheck().check("12345a7890"));
+//    }
+}
